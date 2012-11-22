c66db38ec573476eef106a78958f2172d9a11971
Dan Bennett <naebeth@googlemail.com>
2012-11-22 16:50:28 +0100

    multi-file upload merged from dev branch

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A JavaScript object to make interacting with the HTML5 FileSystem API super-easy!
 
+If you just plan to use this library without modifications, go to the [downloads section](https://github.com/Ultrabenosaurus/HTML5FileSystem/downloads) to get a compiled copy of the latest stable release.
+
 ## Notes ##
 
 All paths should be from root, not relative, unless a `dir` parameter is required in which case all other paths should be relative to `dir`. Also, all paths should be provided as strings, not DirectoryEntry objects.
@@ -12,7 +14,7 @@ Any method that takes `success` and/or `failure` parameters requires these to be
 
 ### Implemented ###
 
-**Generic**
+**Generic** - only `request` is meant to be called directly
 
 * `filesystem.request(type, [size])` - request a FileSystem allocation
 * `filesystem.init(fs)` - set initial variables, read root directory
@@ -39,7 +41,7 @@ Any method that takes `success` and/or `failure` parameters requires these to be
 * `filesystem.file.copy(source, destination)` - copy a file
 * `filesystem.file.move(source, destination)` - move a file
 * `filesystem.file.rename(dir, old, new)` - rename a file
-* `filesystem.file.upload(dir, file, [success, [failure]])` - upload a local file to the FileSystem
+* `filesystem.file.upload(name, dir, [success, [failure]])` - upload one or more local files to the FileSystem
 * `filesystem.file.properties(path, success)` - get the metadata of a file
 
 **URL**
@@ -47,10 +49,15 @@ Any method that takes `success` and/or `failure` parameters requires these to be
 * `filesystem.url.get(path, success)` - get the URL of a file from a path
 * `filesystem.url.resolve(url, success)` - resolve the URL of a file into a path
 
+**Support** - not meant to be called directly
+
+* `filesystem.support.toArray(list)` - convert an object to an array
+* `filesystem.support.listResults(entries, dir)` - output the results of `filesystem.directory.read()` to the webpage
+* `filesystem.support.upload(dir, file, [success, [failure]])` - this method actually performs the upload, called by `filesystem.file.upload()`
+
 ## To Do ##
 
 * Improve `filesystem.errorHandler(e)`
-* Figure out how to upload multiple files at the same time
 * Add support for reading/writing other filetypes (for apps like web-based image editors)
     * Code this in a way that will allow others to easily add new definitions
 * Investigate any other methods that are needed/could be helpful
--- js/filesystem.js
+++ js/filesystem.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	filesystem = FileSystem();
+	filesystem = new FileSystem();
 	filesystem.request(PERSISTENT);
 };
 
@@ -61,7 +61,7 @@ function FileSystem(){
 					folders = path;
 				}
 				if(folders[0] == '.' || folders[0] == ''){
-					folders = toArray(folders);
+					folders = filesystem.support.toArray(folders);
 					folders.shift();
 				}
 				if(!root){
@@ -70,7 +70,7 @@ function FileSystem(){
 				filesystem.root.getDirectory(root, {}, function(rootEntry){
 					rootEntry.getDirectory(folders[0], {create: true}, function(dirEntry){
 						if(folders.length){
-							folders = toArray(folders);
+							folders = filesystem.support.toArray(folders);
 							folders.shift();
 							filesystem.directory.create(folders, dirEntry.fullPath);
 						}
@@ -82,7 +82,7 @@ function FileSystem(){
 					filesystem.errorHandler(e);
 				});
 			},
-			delete:function(path){
+			del:function(path){
 				path = (path.substring(-1) == '/') ? path : path+'/';
 				filesystem.root.getDirectory(path, {create: false}, function(dirEntry) {
 					dirEntry.removeRecursively(function() {
@@ -102,7 +102,7 @@ function FileSystem(){
 					filesystem.dirReader = dirEntry.createReader();
 					filesystem.entries = [];
 					filesystem.dirReader.readEntries(function(results) {
-						listResults(toArray(results).sort(), dirEntry.fullPath); // external function used to display directory list
+						filesystem.support.listResults(filesystem.support.toArray(results).sort(), dirEntry.fullPath);
 					}, function(e){
 						filesystem.errorHandler(e);
 					});
@@ -174,7 +174,7 @@ function FileSystem(){
 				var path = path;
 				var folders = path.split('/');
 				var filename = folders.pop();
-				folders = toArray(folders);
+				folders = filesystem.support.toArray(folders);
 				filesystem.directory.create(folders);
 				filesystem.root.getDirectory(folders.join('/'), {create: false, exclusive: true}, function(dirEntry){
 					dirEntry.getFile(filename, {create: true}, function(fileEntry){
@@ -188,10 +188,10 @@ function FileSystem(){
 					setTimeout(function(){filesystem.file.create(path);}, 50);
 				});
 			},
-			delete:function(path){
+			del:function(path){
 				var folders = path.split('/');
 				var filename = folders.pop();
-				var folder = toArray(folders).join('/')+'/';
+				var folder = filesystem.support.toArray(folders).join('/')+'/';
 				filesystem.root.getFile(path, {create: false}, function(fileEntry) {
 					fileEntry.remove(function() {
 						filesystem.directory.read(folder);
@@ -222,10 +222,10 @@ function FileSystem(){
 					folders = path.split('/'),
 					filename = folders.pop();
 				if(!append){
-					this.delete(path);
+					this.del(path);
 					this.create(path);
 				}
-				folders = toArray(folders);
+				folders = filesystem.support.toArray(folders);
 				filesystem.directory.create(folders);
 				filesystem.root.getDirectory(folders.join('/'), {create: false, exclusive: true}, function(dirEntry){
 					dirEntry.getFile(filename, {create: false, exclusive: true}, function(fileEntry){
@@ -299,42 +299,13 @@ function FileSystem(){
 					filesystem.errorHandler(e);
 				});
 			},
-			upload:function(dir, file, success, failure){
-				if(typeof file !== 'undefined'){
-					var ftype = file.type, fname = file.name, fmod = file.lastModifiedDate, fsize = file.size;
-					filesystem.root.getDirectory(dir, {create: false}, function(dirEntry){
-						filesystem.root.getFile(fname, {create: true}, function(fileEntry){
-							var reader = new FileReader();
-							reader.onloadend = function(theFile){
-								// console.log(theFile);
-								if(theFile.target.readyState == FileReader.DONE){
-									console.log('begin writing');
-									fileEntry.createWriter(function(fileWriter){
-										fileWriter.onwriteend = success || function(e){filesystem.directory.read(dir)};
-										fileWriter.onerror = failure || function(e){
-											filesystem.errorHandler(e);
-										};
-										var blob = new Blob([theFile.target.result], {type: ftype});
-										fileWriter.write(blob);
-									}, function(e){
-										filesystem.errorHandler(e);
-									});
-								}
-							};
-							reader.onerror = failure || function(e){
-								filesystem.errorHandler(e);
-							};
-							if(ftype.match('text.*')) {
-								reader.readAsText(file);
-							} else {
-								reader.readAsArrayBuffer(file);
-							}
-						}, function(e){
-							filesystem.errorHandler(e);
-						});
-					}, function(e){
-						filesystem.errorHandler(e);
-					});
+			upload:function(name, dir, success, failure){
+				inputs = document.getElementsByName(name);
+				for(var i = 0, elem; elem = inputs[i]; i++){
+					f = elem.files[0];
+					if(typeof f !== 'undefined'){
+						filesystem.support.upload(dir, f, success, failure);
+					}
 				}
 			},
 			properties:function(path, success){
@@ -364,7 +335,79 @@ function FileSystem(){
 					filesystem.errorHandler(e);
 				});
 			}
+		},
+		support:{
+			toArray:function(list){
+				return Array.prototype.slice.call(list || [], 0);
+			},
+			listResults:function(entries, dir){
+				dir = (dir == '../') ? "" : dir;
+				dir = (dir.substring(-1) == '/') ? dir : dir+'/';
+				document.querySelector('#filelist').innerHTML = (dir) ? "<strong>"+dir+"</strong>" : "<strong>/</strong>";
+				var fragment = document.createDocumentFragment();
+				if(dir != '/'){
+					var li = document.createElement('li');
+					li.innerHTML = '<img src="images/folder-icon.png" /><span> <a href="javascript: filesystem.directory.read(\''+dir+'../\');">../</a></span>';
+					fragment.appendChild(li);
+				}
+				entries.forEach(function(entry, i) {
+					var img = entry.isDirectory ? '<img src="images/folder-icon.png" />' : '<img src="images/file-icon.png" />';
+					var li = document.createElement('li');
+					if(entry.isDirectory){
+						li.innerHTML = img + '<span> <a href="javascript: filesystem.directory.read(\''+entry.fullPath+'\');">' + entry.name + '</a></span>';
+					} else {
+						li.innerHTML = img + '<span> ' + entry.name + '</span>';
+					}
+					fragment.appendChild(li);
+				});
+				document.querySelector('#filelist').appendChild(fragment);
+			},
+			upload:function(dir, file, success, failure){
+				if(typeof file !== 'undefined'){
+					var ftype = file.type, fname = file.name, fmod = file.lastModifiedDate, fsize = file.size;
+					filesystem.root.getDirectory(dir, {create: false}, function(dirEntry){
+						filesystem.root.getFile(fname, {create: true}, function(fileEntry){
+							var reader = new FileReader();
+							reader.onloadend = function(theFile){
+								if(theFile.target.readyState == FileReader.DONE){
+									fileEntry.createWriter(function(fileWriter){
+										fileWriter.onwriteend = success || function(e){filesystem.directory.read(dir)};
+										fileWriter.onerror = failure || function(e){
+											filesystem.errorHandler(e);
+										};
+										var blob = new Blob([theFile.target.result], {type: ftype});
+										fileWriter.write(blob);
+									}, function(e){
+										filesystem.errorHandler(e);
+									});
+								}
+							};
+							reader.onerror = failure || function(e){
+								filesystem.errorHandler(e);
+							};
+							if(ftype.match('text.*')) {
+								reader.readAsText(file);
+							} else {
+								reader.readAsArrayBuffer(file);
+							}
+						}, function(e){
+							filesystem.errorHandler(e);
+						});
+					}, function(e){
+						filesystem.errorHandler(e);
+						filesystem.directory.create(dir);
+						filesystem.support.upload(dir, file, success, failure);
+					});
+				}
+			}
 		}
 	};
+	if(!Array.prototype.forEach){
+		Array.prototype.forEach = function(fn, scope){
+			for(var i = 0, len = this.length; i < len; ++i){
+				fn.call(scope, this[i], i, this);
+			}
+		}
+	}
 	return filesystem;
 }
